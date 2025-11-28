9823ef1ad5d1cb26865826c4672f6de102544d34
Gamerator190 <gamerator190@gmail.com>
2025-11-28 12:12:48 +0800

    Add (event organizer) dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,6 +9,7 @@ import { MyTicketsComponent } from './pages/my-tickets/my-tickets';
 import { CheckoutComponent } from './pages/checkout/checkout'; // ⬅️ tambahkan ini
 import { ETicketComponent } from './pages/e-ticket/e-ticket';
 import { AdminDashboard } from './pages/admin-dashboard/admin-dashboard';
+import { Dashboard } from './pages/dashboard/dashboard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -16,7 +17,8 @@ export const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'home', component: HomeComponent }, // ⬅️ route baru
 
-  { path: 'admin-dashboard', component: AdminDashboard},
+  { path: 'admin-dashboard', component: AdminDashboard },
+  { path: 'dashboard', component: Dashboard },
 
     // ⬇ TAMBAHKAN INI
   { path: 'movie/:id', component: MovieDetailComponent },
