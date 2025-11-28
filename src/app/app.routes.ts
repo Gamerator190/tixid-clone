92cd320337219f9193e8ded4500e9381a8c8ac39
Gamerator190 <gamerator190@gmail.com>
2025-11-28 12:10:01 +0800

    Add admin dashboard page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,12 +8,15 @@ import { SeatPickerComponent } from './pages/seat-picker/seat-picker';
 import { MyTicketsComponent } from './pages/my-tickets/my-tickets';
 import { CheckoutComponent } from './pages/checkout/checkout'; // ⬅️ tambahkan ini
 import { ETicketComponent } from './pages/e-ticket/e-ticket';
+import { AdminDashboard } from './pages/admin-dashboard/admin-dashboard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'home', component: HomeComponent }, // ⬅️ route baru
+
+  { path: 'admin-dashboard', component: AdminDashboard},
 
     // ⬇ TAMBAHKAN INI
   { path: 'movie/:id', component: MovieDetailComponent },
